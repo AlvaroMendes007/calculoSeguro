ceb42644ff76442ce0d3028e084bf693ad8a4df3
alvaro.mendes <alvaro.mendes@USER1-PC.verreschi.com>
2020-10-04 18:40:05 -0300

    parcelas inseridas com condição de acréscimo, comparação de idade feito com SimpleDateFormat

--- src/main/java/br/com/speedyOfficer/calculoSeguro/api/webService.java
+++ src/main/java/br/com/speedyOfficer/calculoSeguro/api/webService.java
@@ -6,6 +6,12 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
+import java.text.DecimalFormat;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.Period;
+import java.util.Date;
+import java.util.HashMap;
 import java.util.Map;
 
 import org.springframework.http.HttpStatus;
@@ -15,17 +21,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.tempuri.IDescontoservice;
+import org.apache.commons.collections.map.HashedMap;
 import org.json.JSONObject;
 import org.json.XML;
 
 @RestController
 public class webService {
 
-	@RequestMapping(value = "/veiculo/{codVeiculo}")
-	public ResponseEntity<Object> callGet(@PathVariable int codVeiculo) throws IOException {
+	public Double getVeiculoByCod(int codVeiculo) throws IOException {
 
 		URL obj = new URL(
-				"http://www.speedyofficer.com.br/desenvVeiculos/wsRstSpeedyVeiculos_Case.dll/veiculo?cod_veiculo=" + codVeiculo);
+				"http://www.speedyofficer.com.br/desenvVeiculos/wsRstSpeedyVeiculos_Case.dll/veiculo?cod_veiculo="
+						+ codVeiculo);
 		HttpURLConnection con = (HttpURLConnection) obj.openConnection();
 
 		con.setRequestMethod("GET");
@@ -46,40 +53,67 @@ public class webService {
 
 		JSONObject teste = new JSONObject(response.toString());
 		Object teste1 = teste.toMap().get("veiculos");
-		Map<String, Object> teste2 = new JSONObject(teste1.toString().replace("[", "").replace("=", ":").replace(",00", ".00")).toMap();
-		
-		System.out.println(teste2.get("valor_veiculo"));
+		Map<String, Object> teste2 = new JSONObject(
+				teste1.toString().replace("[", "").replace("=", ":").replace(",00", ".00")).toMap();
 
-		return new ResponseEntity<>(teste1, HttpStatus.OK);
+		return (Double) teste2.get("valor_veiculo");
 	}
-	
-	@RequestMapping(value = "/cupom")
-	public String teste() throws IOException {
-			
+
+	@RequestMapping(value = "/cupom/{cupom}")
+	public String getDescontoCupomByCupom(@PathVariable String codCupom) throws IOException {
+
 		IDescontoservice cupom = new IDescontoservice();
-		JSONObject objCupom = (JSONObject) XML.toJSONObject(cupom.getIDescontoPort().obterDesconto("XTGSWI28")).get("cupom");
-		
+		JSONObject objCupom = (JSONObject) XML.toJSONObject(cupom.getIDescontoPort().obterDesconto(codCupom))
+				.get("cupom");
+
 		return objCupom.get("percentualDesconto").toString().replace(",", ".");
 	}
 
 	@RequestMapping(value = "/calculo")
-	public ResponseEntity<Object> calculo(@RequestParam Map<String, String> customParams) throws Exception {
+	public ResponseEntity<Object> calculo(@RequestParam String cpfCnpj, @RequestParam String sexo,
+			@RequestParam String dataNascimento, @RequestParam int codigoVeiculo,
+			@RequestParam(required = false) String codigoCupom) throws Exception {
+
+		JSONObject sexoInvalido = new JSONObject(
+				"{codigoHttp: 400, Mensagem: Verifique o sexo, Dica: Deve estar como masculino ou feminino}");
 		
-		boolean cpfCnpj 	   = customParams.containsKey("cpfCnpj");
-		boolean sexo 		   = customParams.containsKey("sexo");
-		boolean dataNascimento = customParams.containsKey("dataNascimento");
-		boolean codigoVeiculo  = customParams.containsKey("codigoVeiculo");
-		boolean cupom 		   = customParams.containsKey("cupom");
+		SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+		Double valorVeiculo = getVeiculoByCod(codigoVeiculo);
+		Double baseSeguro = valorVeiculo * 0.03;
+		DecimalFormat decimalFormat = new DecimalFormat("####.00");
 		
-		JSONObject respostaInvalida = new JSONObject("{codigoHttp: 400, Mensagem: Verifique os Campos}");
+		String[] split = dataNascimento.split("/");	
 		
-		if (cpfCnpj == false || sexo == false || dataNascimento == false || codigoVeiculo == false) {
-			return new ResponseEntity<>(respostaInvalida.toMap() , HttpStatus.BAD_REQUEST);
+		int dia = Integer.parseInt(split[0]);
+		int mes = Integer.parseInt(split[1]);
+		int ano = Integer.parseInt(split[2]);
+						
+		Period diff = Period.between(LocalDate.of(ano,mes,dia), LocalDate.now());
+		
+		System.out.println(diff.toString().substring(1, 3));
+
+		Double acrescimoParcelas = 0.0;
+		Map<Integer, Double> a = new HashMap<>();
+
+		for (int parcela = 1; parcela <= 12; parcela++) {
+			// a.put(1, (1395 + 1395 * 0.03) / 6);
+			if (parcela >= 6 && parcela <= 9) {
+				acrescimoParcelas = 0.03;
+			}
+			if (parcela > 9) {
+				acrescimoParcelas = 0.05;
+			}
+
+			Double valorTotalSeguro = (baseSeguro + baseSeguro * acrescimoParcelas) / parcela;
+
+			a.put(parcela, (valorTotalSeguro));
 		}
-		else {
-			return null;
-		}
-		
-		
+
+		JSONObject b = new JSONObject("{parcelas: [" + a.toString().replace("=", ":") + "]}");
+
+		return new ResponseEntity<>(b.toMap(), HttpStatus.OK);
+
 	}
+
 }
+
