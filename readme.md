0c42a581a25e60a5324dae6cd7c818de796bff72
alvaro.mendes <alvaro.mendes@USER1-PC.verreschi.com>
2020-10-07 09:36:04 -0300

    readme adicionado

---
+++ readme.md
@@ -0,0 +1,15 @@
+# API REST de Calculo de Seguro
+
+# Tecnologias utilizadas
+
+- [x] Java
+- [x] Framework spring boot
+- [x] Banco de dados mysql
+- [x] Maven
+
+# Como utilizar
+
+- Dar git clone para baixar o projeto.
+- Ir em pom.xml e dar o build para instalar todas dependências necessárias.
+- Rodar em localhost:8080 (se caso a porta ja estiver sendo usada, mudar em src/main/resources/application.properties) para porta desejada.
+- No arquivo tem o path "/" (home) que mostra como devem ser feitas as requisições.
--- src/main/java/br/com/speedyOfficer/calculoSeguro/api/Home.java
+++ src/main/java/br/com/speedyOfficer/calculoSeguro/api/Home.java
@@ -11,8 +11,9 @@ public class Home {
 	public String home() {
 		return   "<html> "
 				+ "	<body style='text-align: center'> "
-				+ "		<h3>Para inserção de cliente basta inserir no corpo da requisição os campos {'cpfCnpj': valor, 'nome': valor, 'dataNascimento': valor, 'sexo': valor} <br><br><br>"
-				+ "		Para inserção de calculo basta inserir no corpo da requisição os campos {'cpfCnpj': valor, 'codigoVeiculo': valor, (Opcional) 'codigoCupom': valor} se caso o cupom estiver expirado não haverá desconto!</h3>"
+				+ "		<h3>(Metódo POST) - Para inserção de cliente path('/cliente') basta inserir no corpo da requisição os campos {'cpfCnpj': valor, 'nome': valor, 'dataNascimento': valor, 'sexo': valor} <br><br><br>"
+				+ "		(Metódo POST) - Para inserção de calculo path ('/calculo') basta inserir no corpo da requisição os campos {'cpfCnpj': valor, 'codigoVeiculo': valor, (Opcional) 'codigoCupom': valor} se caso o cupom estiver expirado não haverá desconto!<br><br><br>"
+				+ "     (Metódo GET) - Para consulta de todos calculos path ('/calculos') </h3>"
 				+ " </body> "
 				+"</html>";
 	}
